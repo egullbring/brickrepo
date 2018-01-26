8d6346e2e789f8d84ba4cecf2b3e2c4aa93a6836
egullbring <erik@gullbring.se>
2018-01-26 11:38:05 +0100

    Test commit

---
+++ notebooks/FirstNoteBook.scala
@@ -0,0 +1,13 @@
+// Databricks notebook source
+spark.conf.set("dfs.adls.oauth2.access.token.provider.type", "ClientCredential")
+spark.conf.set("dfs.adls.oauth2.client.id", "47939dd7-a374-45f9-aa5d-bd6f71b6fbfa")
+spark.conf.set("dfs.adls.oauth2.credential", "3diJM/ZUT8Emq+HcaPsjNJXZ8gvQpDjsvQKit8QJMYE=")
+spark.conf.set("dfs.adls.oauth2.refresh.url", "https://login.microsoftonline.com/microsoft.onmicrosoft.com/oauth2/token")
+
+
+// COMMAND ----------
+
+val df = spark.read.csv("adl://dlstoreergullbr.azuredatalakestore.net/test/Imed2011.csv")
+dbutils.fs.ls("adl://dlstoreergullbr.azuredatalakestore.net/test/Imed2011.csv")
+//val df = spark.read.parquet("adl://dlstoreergullbr.azuredatalakestore.net/output/hiveergullbr/example/data/people.seq")
+//dbutils.fs.ls("adl://dlstoreergullbr.azuredatalakestore.net/output/hiveergullbr/example/data/people.seq")
